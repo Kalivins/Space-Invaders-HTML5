7b50a693b758faa1cf18ed5dcbcd98b22143ac95
Kevin Villard <kevin.v@codeur.online>
2017-10-23 21:31:01 +0200

    test object

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,57 @@ var pos = ship.getBoundingClientRect().left;
 document.addEventListener('keydown', move);
 
 function move(event) {
-console.log(ship.getBoundingClientRect().left);
-	
-/*    Touche droite*/
+    console.log(ship.getBoundingClientRect().left);
+
+    /*    Touche droite*/
     if ((event.keyCode == 39) && pos < (document.body.clientWidth - ship.getBoundingClientRect().width)) {
-        pos +=10;
+        pos += 10;
         ship.style.left = pos + "px";
     }
-	
-/*Touche gauche*/
-    if ((event.keyCode == 37) && pos > 0){
-         pos -=10;
+
+    /*Touche gauche*/
+    if ((event.keyCode == 37) && pos > 0) {
+        pos -= 10;
         ship.style.left = pos + "px";
     }
 }
+//On défini notre fonction objet pour les aliens 
+function Sprite(file, left, top) {
 
-
-
-
+    var alienContainer = document.querySelector(".AlienDiv");
+    this._node = document.createElement("img");
+    this._node.src = file;
+    alienContainer.appendChild(this._node);
+    
+Object.defineProperty(this, "left", {
+    get: function () {
+        return this._left;
+    },
+    set: function (value) {
+        this._left = value;
+        this._node.style.left = value + "px";
+    }
+});
+Object.defineProperty(this, "top", {
+    get: function () {
+        return this._top;
+    },
+    set: function (value) {
+        this._top = value;
+        this._node.style.top = value + "px";
+    }
+});
+Object.defineProperty(this, "display", {
+    get: function() {
+    return this._node.style.display;
+},
+    set: function(value){
+    return this._node.style.display = value;
+}
+});
+    
+this.left = left;
+this.top = top;
+    
+}
+var Alien1 = new Sprite("../img/alien_1.svg", 100, 100);
