ff58d3b7f8deb0ad01c6fc3cd34fa4968ce7a5e7
CharlieMo <charlie.m@codeur.online>
2017-10-23 16:46:25 +0200

    rectangle qui bouge

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*document.onkeydown = function( event) {
+	console.log(event.keyCode);;
+}*/
+
+var ship = document.querySelector('#ship');
+var shipBase = window.getComputedStyle(ship, null).getPropertyValue('left');
+var droite = 10;
+var gauche = -10;
+var pos = ship.getBoundingClientRect().left;
+
+document.addEventListener('keydown', move);
+
+function move() {
+console.log(ship.getBoundingClientRect().left);
+/*    Touche droite*/
+    if (event.keyCode == 39) {
+        pos +=10;
+        ship.style.left = pos + "px";
+    }
+/*Touche gauche*/
+    if (event.keyCode == 37){
+         pos -=10;
+        ship.style.left = pos + "px";
+    }
+}
+
